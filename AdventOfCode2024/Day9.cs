01de44fa30c70de5462061532308030daea2186e
Carrie de Lacy <carrieoliver@googlemail.com>
2024-12-10 09:09:13 +0000

    Nearly day 9

--- AdventOfCode2024/Day9.cs
+++ AdventOfCode2024/Day9.cs
@@ -17,7 +17,7 @@ namespace AdventOfCode {
                 for (int j = 0; j < diskMap[i]; j++){
                     var newIdNumber = new IdNumber {
                         isEmpty = i % 2 == 1,
-                        idNumber = i % 2 == 1 ? null: count % 10
+                        idNumber = i % 2 == 1 ? null: count
                     };
 
                     id.Add(newIdNumber);
@@ -43,6 +43,8 @@ namespace AdventOfCode {
                     numberOfTheEnd -= 1;
                 }
 
+                 if (output.Count > numberOfTheEnd) break;
+
                 id[numberOfTheEnd].position = i;
                 output.Add(id[numberOfTheEnd]);
                 numberOfTheEnd--;
@@ -50,17 +52,69 @@ namespace AdventOfCode {
 
             Int64? total = output.Where(o => !o.isEmpty).Sum(o => o.checkSum);
             return total.ToString();
-            //88957760693
-            //24044850307019
-            //20075802679
-            //5535195185
-            //5534897585
         }
 
         public string SolveB() {
+            var charArray = Input[0].ToCharArray();
+            var diskMap = charArray.Select(s => (int)Char.GetNumericValue(s)).ToArray();
+            var files = new List<DiskFile>();
+            var count = 0;
+            for (int i = 0; i < diskMap.Length; i++) {
+                var newIdNumber = new DiskFile {
+                    idNumber = i % 2 == 1 ? null: count,
+                    size = diskMap[i]
+                };
+
+                files.Add(newIdNumber);
+
+                if (i % 2 == 0) {
+                    count++;
+                }
+            }
+
+            var defragedFiles = new List<DiskFile>();
+            for (int i = 0; i < files.Count; i++) {
+                if (i > files.Count) break;
+
+                var numberOfTheEnd = files.Count - 1;
+
+                if (files[i].idNumber.HasValue){
+                    defragedFiles.Add(files[i]);
+                    continue;
+                }
+
+                var spaceToFill = files[i].size;
+                var numberOfFileToMove = files.Count - 1;
+                var filesToMove = new List<DiskFile>();
+                while (spaceToFill > 0) {
+                    if (files[numberOfFileToMove].idNumber != null && files[numberOfFileToMove].size <= spaceToFill && !filesToMove.Contains(files[numberOfFileToMove])) {
+                        spaceToFill -= files[numberOfFileToMove].size;
+                        filesToMove.Add(files[numberOfFileToMove]);
+                        numberOfFileToMove = files.Count - 1;
+                    }
+                    else {
+                        numberOfFileToMove -= 1;
+                    }
+                }
+
+                defragedFiles.AddRange(filesToMove);
+                files.Except(filesToMove);
+            }
+
+            var position = 0;
+            Int64 checkSum = 0;
+            foreach (var file in defragedFiles)
+            {
+                for (int i = 0; i < file.size; i++)
+                {
+                    if (file.idNumber.HasValue){
+                    checkSum += position * file.idNumber.Value;
+                    }
+                    position++;
+                }
+            }
             
-            
-            return "";
+            return checkSum.ToString();
         }
 
     }
@@ -73,5 +127,11 @@ namespace AdventOfCode {
         public int position {get; set;}
 
         public Int64? checkSum => idNumber.HasValue ? idNumber.Value * position : null;
+    }
+
+    class DiskFile(){
+         public Int64? idNumber {get; set;}
+
+         public int size{get; set;}
     }
 }
